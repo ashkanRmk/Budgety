aff8a65394cf1c40dfe445ef444186fb70e7bf84
Samane <baran.s2s.26.75@gmail.com>
2017-08-23 02:46:24 +0430

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+//Budget Controller
+var budgetController = (function () {
+
+    var Expenses = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var data = {
+        allItems: {
+            exp : [],
+            inc : []
+        },
+        total: {
+            totalInc : 0,
+            totalExp : 0
+        }
+    }
+
+
+
+
+})();
+
+
+//UI Controller
+var uiController = (function () {
+    var uiStrings = {
+        typeString: '.add__type',
+        descstring: '.add__description',
+        valueString: '.add__value',
+        btnString: '.add__btn',
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(uiStrings.typeString).value,
+                desc: document.querySelector(uiStrings.descstring).value,
+                value: document.querySelector(uiStrings.valueString).value
+            };
+        },
+        getUiStrings: function () {
+            return uiStrings;
+        }
+
+    };
+
+
+
+})();
+
+
+//Global Controller
+var controller = (function (budgetCntrl, uiCntrl) {
+
+    var eventListener = function () {
+        var getUiStrings = uiCntrl.getUiStrings();
+        document.querySelector(getUiStrings.btnString).addEventListener('click', addNewItem);
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13)
+                addNewItem();
+        });
+    };
+
+    var addNewItem = function () {
+        uiCntrl.getInput();
+
+
+        //1. Get input
+
+        //2. Add item to budget controller
+
+        //3. Add item to UI
+
+        //4. Calculate budget
+
+        //5. update Budget UI
+    };
+
+
+    return {
+        init: function () {
+            console.log('setup');
+            eventListener();
+        }
+    };
+})(budgetController, uiController);
+
+controller.init();
