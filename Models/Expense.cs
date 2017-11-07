97b6396845cc1ee381d610405f3b4743074025ee
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-11-07 15:09:13 +0330

    Initial Models

---
+++ Models/Expense.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Budgety_master.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Caption { get; set; }
+
+        public int UserId { get; set; }
+
+        public double Amount { get; set; }
+        
+        public DateTime DateAdded { get; set; }
+    }
+}
